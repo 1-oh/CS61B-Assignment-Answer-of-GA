da0558094a2a173f2b1d5009cc49180ac7ca9b6d
Shiqiang Wu <3262109672@qq.com>
2025-04-01 21:32:08 +0800

    Add files via upload

---
+++ proj0/Nbody/README.md
@@ -0,0 +1,3 @@
+# Nbody
+ CS61B Project0 for creating a simulating program of the Nbody problem.
+ Finished by **Gary Agasa** in Feb,2025
---
+++ proj0/tryfordraw.java
@@ -0,0 +1,9 @@
+public class tryfordraw {
+    public static void main(String[] args){
+        StdDraw.setScale(-256,256);
+        StdDraw.clear();
+        StdDraw.picture(0, 0,"images/starfield.jpg");
+        StdDraw.show();
+        StdDraw.pause(2000);
+    }
+}
