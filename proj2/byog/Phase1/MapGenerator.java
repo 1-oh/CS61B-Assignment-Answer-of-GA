12a905cc80c41de0ac832bc4c6029c71f7846f33
Shiqiang Wu <3262109672@qq.com>
2025-04-01 23:17:47 +0800

    Update MapGenerator.java

--- proj2/byog/Phase1/MapGenerator.java
+++ proj2/byog/Phase1/MapGenerator.java
@@ -11,6 +11,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
+import edu.princeton.cs.introcs.StdDraw;
+
+
 public class MapGenerator {
        public static long SEED= Game.seed;
        public static int width;
